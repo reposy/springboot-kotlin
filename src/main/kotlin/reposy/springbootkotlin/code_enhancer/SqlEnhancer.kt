924b778697286d40b1f58868a7cc4236d9b1de7b
syhan <hsykys0728@gmail.com>
2024-10-17 23:54:16 +0900

    processSqlForSplit(문자열 어절 단위 분리) - '\'', ' ', '\n', '\t' 처리. 가다듬어야함

--- src/main/kotlin/reposy/springbootkotlin/code_enhancer/SqlEnhancer.kt
+++ src/main/kotlin/reposy/springbootkotlin/code_enhancer/SqlEnhancer.kt
@@ -17,47 +17,61 @@ class SqlEnhancer {
         return ""
     }
 
-    fun processSqlForSplit(sql: String): String {
+    fun processSqlForSplit(sql: String): List<String> {
 
-        val result = StringBuilder()
+        val result = ArrayList<String>()
 
         var isInQuote = false
-        var openBracketCount = 0
+        var openParenthesesCount = 0 // '(': +1, ')': -1
+        var openSquareBracketsCount = 0 // '[': +1, ']': -1
+        var continuousSpaceCount = 0
+
         var left = 0
         for ( right in sql.indices ) {
             val c = sql[right]
-            when(c) {
-                '\'' -> {
-                    isInQuote = !isInQuote
-                    if (!isInQuote) {
-                        result.append(sql.substring(left, right+1))
-                        left = right + 1
-                    }
-                }
-                '(' -> {
-                    if(!isInQuote)
-                        openBracketCount++
-                }
-                ')' -> {
-                    if(!isInQuote)
-                        openBracketCount--
-                }
-                '\n', '\t' -> {
-                    if ( left < right ) {
-
-                    }
-                    result.append(' ')
-
+            if ( isInQuote ) { // 문자열 내의 경우
+                if ( c == '\'' ) { // 문자열이 종료에 다다랐을 경우
+                    isInQuote = false
+                    val tempStr = sql.substring(left, right + 1)
+                    if (tempStr.trim().isNotEmpty())
+                        result.add(tempStr)
                     left = right + 1
                 }
+            }
+            else { // 문자열 밖
+                if ( c in listOf(' ', '\n', '\t') ) {
+                    if ( right == sql.lastIndex ) { // 마지막 문자가 공백인 경우
+                        val tempStr = sql.substring(left, right + 1)
+                        if (tempStr.trim().isNotEmpty())
+                            result.add(tempStr)
+                    }
+                }
+                else {
+                    if ( continuousSpaceCount > 0 ) {
+                        result.add(sql.substring(left, right - continuousSpaceCount))
+                        continuousSpaceCount = 0
+                        left = right
+                    } // if
+                    else {
+                        continuousSpaceCount = 0
+                        when(c) {
+                            '\'' -> isInQuote = true
+                            '(' -> openParenthesesCount++
+                            ')' -> openParenthesesCount--
+                            '[' -> openSquareBracketsCount++
+                            ']' -> openSquareBracketsCount--
+                            ' ', '\n', '\t' -> {
+                                continuousSpaceCount++
+                            }
+                        } // else-when
+                    } // else-else
+                } // else
 
             }
-            if ( !isInQuote && openBracketCount == 0 ) {
 
-            }
-        }
+        } // for ( right in sql.indices )
 
-        return result.toString()
+        return result
     }
 
     fun splitIntoWords(input: String): List<String> {
